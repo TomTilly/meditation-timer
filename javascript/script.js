850eae1be269ab8331187bbfc89abbf212947075
Tom Tilly <ny.prosource@gmail.com>
2018-10-05 18:05:27 -0400

    Added timer logic

--- javascript/script.js
+++ javascript/script.js
@@ -5,7 +5,10 @@ $(document).ready(function(){
 	var $minsInput = $('input[type="text"]:last-of-type');
 	var $hrsDisplay = $('.hours-remaining');
 	var $minsDisplay = $('.mins-remaining');
+	var $secondsDisplay = $('.seconds-remaining');
+	var $hrsColon = $('.hours-remaining + span');
 	var hours, minutes; // Values submitted by form
+	var seconds = 0;
 
 	// Open sidebar
 	$('nav a').on('click', function(e){
@@ -43,13 +46,74 @@ $(document).ready(function(){
 			$('.timer-wrapper h2').addClass('hide');
 			$('.timer-wrapper form').addClass('hide');
 			$('.timer').removeClass('hide');
-
+			startTimer();
 		}
 	});
 
-	function startTimer(hours, minutes){
+	function startTimer(){
+		console.log('startTimer called');
 		if (hours === 0) {
+			$hrsDisplay.text('');
+			$hrsColon.text('');
+		} else {
+			$hrsDisplay.text(hours.toString());
+		}
+		if (minutes === 0) {
+			$minsDisplay.text('00');
+		} else {
+			$minsDisplay.text(minutes.toString());
+		}
+		$secondsDisplay.text('00');
+		
+		var intervalID = setInterval(function(){
+			// hours = Number($hrsDisplay.text());
+			// minutes = Number(minsDisplay.text());
+			// seconds = Number(secondsDisplay.text());
+			if (hours === 0 && minutes === 0 && seconds === 0){
+				clearInterval(intervalID);
+				console.log('timer stopped');
+			} else {
+				decrementTimer();
+			}
+		},1000);
+	}
 
+	function decrementTimer(){
+		console.log('decrementTimer called');
+		if (seconds === 0 ){
+			if(minutes === 0){
+				if (hours > 1){
+					hours--;
+					$hrsDisplay.text(hours.toString());
+					minutes = 59;
+					$minsDisplay.text(minutes.toString());
+					seconds = 59;
+					$secondsDisplay.text(seconds.toString());
+				} else if (hours === 1){
+					hours--;
+					$hrsDisplay.text('');
+					$hrsColon.text('');
+					minutes = 59;
+					$minsDisplay.text(minutes.toString());
+					seconds = 59;
+					$secondsDisplay.text(seconds.toString());
+				} else  {
+					minutes = 0;
+					$minsDisplay.text(minutes.toString());
+				}
+			} else {
+				minutes--;
+				$minsDisplay.text(minutes.toString());
+				seconds = 59;
+				$secondsDisplay.text(seconds.toString());
+			}
+		} else {
+			seconds--;
+			if (seconds < 10){
+				$secondsDisplay.text('0' + seconds.toString());
+			} else {
+				$secondsDisplay.text(seconds.toString());	
+			}
 		}
 	}
 });
